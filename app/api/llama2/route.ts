36f685eaf5ade62cbffa815f5500d07ebd75501e
Lars Grammel <lars.grammel@gmail.com>
2023-11-19 16:20:00 +0100

    Simplify.

--- app/api/llama2/route.ts
+++ app/api/llama2/route.ts
@@ -11,6 +11,7 @@ export const runtime = "edge";
 export async function POST(req: Request) {
   const { messages }: { messages: Message[] } = await req.json();
 
+  // Use ModelFusion to call Ollama:
   const textStream = await streamText(
     new OllamaTextGenerationModel({
       model: "llama2:7b-chat",
@@ -24,17 +25,13 @@ export async function POST(req: Request) {
         "Follow the user's instructions carefully.",
 
       // map Vercel AI SDK Message to ModelFusion ChatMessage:
-      messages: messages
-        .filter(
-          // only user and assistant roles are supported:
-          (message) => message.role === "user" || message.role === "assistant"
-        )
-        .map((message) => ({
-          role: message.role,
-          content: message.content,
-        })) as ChatMessage[],
+      messages: messages.filter(
+        // only user and assistant roles are supported:
+        (message) => message.role === "user" || message.role === "assistant"
+      ) as ChatMessage[],
     }
   );
 
+  // Return the result using the Vercel AI SDK:
   return new StreamingTextResponse(readableFromAsyncIterable(textStream));
 }
